31eaefc5215c689a3db25d7d1a950faa4a1529c4
Md Irfan <31414222+mdirfan121@users.noreply.github.com>
2021-06-16 21:08:49 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mockinterview/Utilities/ApiUrl.java
@@ -0,0 +1,12 @@
+package com.example.mockinterview.Utilities;
+
+public class ApiUrl {
+    private static final String BASE_URL = "https://minibank.tech/ScalerAssignment/";
+    public static final String ALL_MEETINGS = BASE_URL+"AllMeeting.php";
+    public static final String ALL_UNSCHEDULE = BASE_URL+"ToScheduleMeeting.php";
+    public static final String ADD_MEETING = BASE_URL+"AddMeeting.php";
+    public static final String UPDATE_MEETING = BASE_URL+"UpdateMeeting.php";
+    public static final String DELETE_MEETING = BASE_URL+"delete.php";
+
+
+}
